63d8f95f760f12690412dd2a59e995a252281b1b
SoySa <SoySa@DESKTOP-QTPJCB7>
2026-02-19 18:26:45 -0500

    MultipleNumberOfAnother

---
+++ logic-concept-SHB/MultipleNumberOfAnother/Program.cs
@@ -0,0 +1,21 @@
+﻿using Share;
+var answer = string.Empty;
+var options = new List<string> { "si", "no" };
+{
+    var a = ConsoleExtension.GetInt("Ingrese el primer numero: ");
+    var b = ConsoleExtension.GetInt("Ingrese el segundo numero: ");
+    if (b % a == 0)
+    {
+        Console.WriteLine($"{a} es multiplo de {b}");
+    }
+    else
+    {
+        Console.WriteLine($"{a} no  es multiplo de {b}");
+
+    }
+
+    do
+    {
+        answer = ConsoleExtension.GetValidOptions("Desea continuar Si , No?: ", options);
+    } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
