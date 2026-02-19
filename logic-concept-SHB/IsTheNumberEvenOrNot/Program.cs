bc4a736edd16dc1638d6e38de990feb48ee0a256
SoySa <SoySa@DESKTOP-QTPJCB7>
2026-02-19 12:29:49 -0500

    IsTheNumberEvenOrNot

---
+++ logic-concept-SHB/IsTheNumberEvenOrNot/Program.cs
@@ -0,0 +1,35 @@
+﻿var numberString = string.Empty;
+do
+{
+    Console.Write("Ingrese Numero o presione 'Salir' para salir de la ejecucion:");
+    numberString = Console.ReadLine();
+
+
+    if (numberString!.ToLower() == "salir")
+    {
+        continue;
+    }
+    var numberInt = 0;
+
+    if (int.TryParse(numberString, out numberInt))
+    {
+        if (numberInt % 2 == 0)
+        {
+            Console.WriteLine($"el numero {numberInt} es par.");
+        }
+        else
+        {
+            Console.WriteLine($"El numero {numberInt} es Impar.");
+        }
+
+    }
+    else
+    {
+        Console.WriteLine($"el valor que dijistate {numberString}, no es un numero entero.");
+    }
+
+
+
+
+} while (numberString!.ToLower() != "salir");
+Console.WriteLine("Excelente");
