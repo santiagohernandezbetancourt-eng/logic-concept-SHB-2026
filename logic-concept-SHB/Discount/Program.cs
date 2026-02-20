e5cffa8c001168200c66f1d2e54bfaca217b1526
SoySa <SoySa@DESKTOP-QTPJCB7>
2026-02-19 19:35:31 -0500

    Discount

---
+++ logic-concept-SHB/Discount/Program.cs
@@ -0,0 +1,42 @@
+﻿using Share;
+
+var answer = string.Empty;
+var options = new List<string> { "si", "no" };
+
+do
+{
+    var Num_desks = ConsoleExtension.GetInt("Ingrese el numero de escritorios a comprar: ");
+
+    var Pay_total = CalculateValue(Num_desks);
+    Console.WriteLine($"El valor total a pagar es: {Pay_total:C2}");
+
+
+
+    do
+    {
+        answer = ConsoleExtension.GetValidOptions("Desea continuar si , no?: ", options);
+    } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
+
+decimal CalculateValue(int Num_desks)
+{
+    float discount = 0f;
+    if (Num_desks < 5)
+    {
+        discount = 0.1f;
+    }
+    else if (Num_desks >= 10)
+    {
+        discount = 0.2f;
+    }
+    else
+    {
+        discount = 0.4f;
+    }
+
+    return Num_desks * 650000M * (decimal)(1 - discount);
+
+}
+
+Console.WriteLine("Excelente");
---
+++ logic-concept-SHB/PayRoll/Program.cs
@@ -0,0 +1,36 @@
+﻿using Share;
+var answer = string.Empty;
+var options = new List<string> { "si", "no" };
+
+do
+{
+    var name = ConsoleExtension.GetString("ingrese su nombre ");
+    var lastName = ConsoleExtension.GetString("ingrese su apellido ");
+
+    var H_Work = ConsoleExtension.GetFloat("ingrese las horas trabajadas ");
+
+    var V_hour = ConsoleExtension.GetDecimal("ingrese el valor por hora ");
+
+    var min_salary = ConsoleExtension.GetDecimal("ingrese el salario minimo legal vigente ");
+
+
+    var salary = (decimal)H_Work * V_hour;   
+
+    if (min_salary >= salary)
+    {
+        Console.WriteLine($"nombre  .... :{name} {lastName}");
+        Console.WriteLine($"salario .... :{salary:C2}");
+    }
+    else
+    {
+        Console.WriteLine($"nombre  .... :{name} {lastName}");
+        Console.WriteLine($"salario .... :{min_salary:C2}");
+
+    }
+    do
+    {
+        answer = ConsoleExtension.GetValidOptions("Desea continuar [S]i , [N]o?: ", options);
+    } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+
+} while (answer!.Equals("si", StringComparison.CurrentCultureIgnoreCase));
+Console.WriteLine("Amen");
